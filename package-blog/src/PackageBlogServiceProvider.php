67c0feb296dd8b120ca7e08fb3f597a5b833c17a
Muhammad Kazim <muhammad@seamlesshr.com>
2022-01-28 22:09:34 +0100

    created Package composer and Service provider

---
+++ package-blog/src/PackageBlogServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Skillshare\PackageBlog;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class PackageBlogServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+}
