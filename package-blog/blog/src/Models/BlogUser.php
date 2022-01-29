8e214dbfa663ba8228e9ad153fd1bfe3f552cdbb
Muhammad Kazim <muhammad@seamlesshr.com>
2022-01-29 21:02:29 +0100

    created BlogUser Model

---
+++ package-blog/blog/src/Models/BlogUser.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Skillshare\PackageBlog\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class BlogUser extends Model
+{
+
+    // protected $table = 'blog.users';
+
+    protected $fillable = [];
+}
