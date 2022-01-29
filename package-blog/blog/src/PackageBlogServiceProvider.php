4dad5c076aa15908cba0ac35eb85aab8ba8bd7f7
Muhammad Kazim <muhammad@seamlesshr.com>
2022-01-29 21:31:32 +0100

    deleted default migration files

--- package-blog/blog/src/PackageBlogServiceProvider.php
+++ package-blog/blog/src/PackageBlogServiceProvider.php
@@ -12,6 +12,8 @@ class PackageBlogServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        
     }
 
     public function register()
