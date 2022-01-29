7aedc69815aec6c2020760a82928d5f3ed11ab38
Muhammad Kazim <muhammad@seamlesshr.com>
2022-01-29 21:03:10 +0100

    created Resource controller for blog user

--- package-blog/blog/src/PackageBlogServiceProvider.php
+++ package-blog/blog/src/PackageBlogServiceProvider.php
@@ -11,7 +11,7 @@ class PackageBlogServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/routes/api.php');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
     }
 
     public function register()
