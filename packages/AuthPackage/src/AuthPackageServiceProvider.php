881f6750ac33eb2e0add19aa9c066fa61556e1d6
Muhammad Kazim <muhammad@seamlesshr.com>
2022-01-30 12:16:07 +0100

    registerted migration and views for the Auth Package

--- packages/AuthPackage/src/AuthPackageServiceProvider.php
+++ packages/AuthPackage/src/AuthPackageServiceProvider.php
@@ -10,6 +10,8 @@ class AuthPackageServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'authPackage');
     }
 
     public function register()
